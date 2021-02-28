f65bf9c4873e55d3bbc077cccee8513d0f4ce7c2
Pascal <pascal.bertrand85@laposte.net>
2021-02-28 21:52:32 +0100

    added one tab for activities

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./modules/tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'tab4',
+    loadChildren: () => import('./modules/tab4/tab4.module').then( m => m.Tab4PageModule)
   }
 ];
 @NgModule({
