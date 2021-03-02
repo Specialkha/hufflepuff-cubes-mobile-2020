24e1c6dc548e29fa87f0c349e929ac65faf906f0
Elouan Marquet <elouanmarquet@gmail.com>
2021-03-02 18:20:15 +0100

    front

--- src/app/modules/tab2/tab2.page.ts
+++ src/app/modules/tab2/tab2.page.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { HttpBlogService } from 'src/app/core/http/blog/httpBlog.service';
 import { Blog } from 'src/app/core/model/blog';
+import { AuthService } from 'src/app/core/services/auth.service';
+
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
+
 export class Tab2Page {
   blogs: Blog;
 
-  constructor(private blogHttp: HttpBlogService) {
+  constructor(private blogHttp: HttpBlogService, public auth: AuthService, private router: Router) {
     blogHttp.getBlogs().subscribe((data: any) => {
       console.log(data);
       this.blogs = data;
