af72310001d1a15330b3612f4cd6e7abe7109684
Pascal <pascal.bertrand85@laposte.net>
2021-03-03 07:47:11 +0100

    creation blog done

--- src/app/modules/components/blog-creation/blog-creation.component.ts
+++ src/app/modules/components/blog-creation/blog-creation.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpBlogService } from 'src/app/core/http/blog/httpBlog.service';
+import { HttpUserService } from 'src/app/core/http/user/httpUser.service';
+import { User } from 'src/app/core/model/user';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-blog-creation',
@@ -7,8 +13,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogCreationComponent implements OnInit {
 
-  constructor() { }
+  blogCreationForm: FormGroup;
 
-  ngOnInit() {}
+  constructor(private userHttp: HttpUserService, private httpBlog: HttpBlogService, private httpUser: HttpUserService, private auth: AuthService, private router: Router) { }
+
+  ngOnInit() {
+    this.blogCreationForm = this.createNewBlogFormGroup();
+  }
+
+  createNewBlogFormGroup() {
+    return new FormGroup({
+      title: new FormControl('', Validators.required),
+      headline: new FormControl('', [Validators.required, Validators.maxLength(100)]),
+      description: new FormControl('', [Validators.required, Validators.maxLength(500)])
+    });
+  }
+
+  async createNewBlog() {
+    let userId: string;
+    await this.userHttp.getUserWithToken(this.auth.authToken).subscribe((user: User) => {
+      userId = user._id;
+      const payload = {
+        title: this.blogCreationForm.value.title,
+        headline: this.blogCreationForm.value.headline,
+        description: this.blogCreationForm.value.description,
+        authorId: userId
+      }
+      this.httpBlog.createNewBlog(payload).subscribe((data: any) => {
+        if (data) {
+          this.router.navigate(['/blog', data._id]);
+        }
+      });
+    });
+  }
 
 }
--- src/app/modules/tab2/tab2.module.ts
+++ src/app/modules/tab2/tab2.module.ts
@@ -2,7 +2,7 @@ import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
@@ -15,6 +15,7 @@ import { BlogCreationComponent } from '../components/blog-creation/blog-creation
     IonicModule,
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     ExploreContainerComponentModule,
     Tab2PageRoutingModule,
     FilterModule
