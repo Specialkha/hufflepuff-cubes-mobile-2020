3679078f14da4873c21338c2b1d66eb1d692d4d2
Pascal <pascal.bertrand85@laposte.net>
2021-03-04 19:11:17 +0100

    post creation component import

--- src/app/modules/tab6/tab6.module.ts
+++ src/app/modules/tab6/tab6.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -9,14 +9,16 @@ import { Tab6PageRoutingModule } from './tab6-routing.module';
 import { Tab6Page } from './tab6.page';
 import { BlogComponent } from '../components/blog/blog.component';
 import { PostComponent } from '../components/post/post.component';
+import { PostCreationComponent } from '../components/post-creation/post-creation.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    ReactiveFormsModule,
     Tab6PageRoutingModule
   ],
-  declarations: [Tab6Page, BlogComponent]
+  declarations: [Tab6Page, BlogComponent, PostCreationComponent]
 })
-export class Tab6PageModule {}
+export class Tab6PageModule { }
